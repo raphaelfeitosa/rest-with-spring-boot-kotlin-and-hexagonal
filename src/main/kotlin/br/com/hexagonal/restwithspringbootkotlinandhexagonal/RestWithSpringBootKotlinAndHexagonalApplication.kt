c5a366260fb2b3e5032db5ef06948bf9cabfe1f5
Raphael Feitosa <raphaelcs2@gmail.com>
2022-09-16 16:42:25 -0300

    Adding project

---
+++ src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/RestWithSpringBootKotlinAndHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package br.com.hexagonal.restwithspringbootkotlinandhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestWithSpringBootKotlinAndHexagonalApplication
+
+fun main(args: Array<String>) {
+	runApplication<RestWithSpringBootKotlinAndHexagonalApplication>(*args)
+}
