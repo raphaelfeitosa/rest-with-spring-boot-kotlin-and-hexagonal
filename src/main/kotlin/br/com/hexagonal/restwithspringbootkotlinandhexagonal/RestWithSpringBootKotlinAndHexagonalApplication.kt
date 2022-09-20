bce2dea8aa49a827b60b3f09ad9b7b10ace2fbdd
Raphael Feitosa <raphaelcs2@gmail.com>
2022-09-20 17:13:56 -0300

    updating properties(DB, api via cep) and enabled feign client

--- src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/RestWithSpringBootKotlinAndHexagonalApplication.kt
+++ src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/RestWithSpringBootKotlinAndHexagonalApplication.kt
@@ -2,8 +2,10 @@ package br.com.hexagonal.restwithspringbootkotlinandhexagonal
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 
 @SpringBootApplication
+@EnableFeignClients
 class RestWithSpringBootKotlinAndHexagonalApplication
 
 fun main(args: Array<String>) {
