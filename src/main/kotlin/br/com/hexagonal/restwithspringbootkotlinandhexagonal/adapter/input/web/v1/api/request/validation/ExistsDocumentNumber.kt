aaee0c43d76ce621f8fc19f1d2ddff8b32aab1b7
Raphael Feitosa <raphaelcs2@gmail.com>
2022-09-26 22:51:45 -0300

    adding validation document number exists

---
+++ src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/adapter/input/web/v1/api/request/validation/ExistsDocumentNumber.kt
@@ -0,0 +1,39 @@
+package br.com.hexagonal.restwithspringbootkotlinandhexagonal.adapter.input.web.v1.api.request.validation
+
+import br.com.hexagonal.restwithspringbootkotlinandhexagonal.adapter.input.web.v1.api.errors.Errors.DOCUMENT_NUMBER_ALREADY_EXISTS
+import br.com.hexagonal.restwithspringbootkotlinandhexagonal.adapter.input.web.v1.api.exceptions.BusinessException
+import br.com.hexagonal.restwithspringbootkotlinandhexagonal.adapter.output.postgresql.repository.ClientRepository
+import jakarta.validation.Constraint
+import jakarta.validation.ConstraintValidator
+import jakarta.validation.ConstraintValidatorContext
+import jakarta.validation.Payload
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+import kotlin.reflect.KClass
+
+
+@MustBeDocumented
+@Constraint(validatedBy = [ExistsDocumentNumberValidator::class])
+@Target(AnnotationTarget.FIELD)
+@Retention(AnnotationRetention.RUNTIME)
+@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+annotation class ExistsDocumentNumber(
+    val message: String = "",
+    val groups: Array<KClass<Any>> = [],
+    val payload: Array<KClass<Payload>> = [],
+)
+
+class ExistsDocumentNumberValidator(
+    private val clientRepository: ClientRepository,
+) : ConstraintValidator<ExistsDocumentNumber, String> {
+
+    override fun isValid(documentNumber: String?, context: ConstraintValidatorContext?): Boolean {
+        when {
+            documentNumber != null ->
+                if (clientRepository.findByDocumentNumber(documentNumber).isPresent)
+                    throw BusinessException(DOCUMENT_NUMBER_ALREADY_EXISTS)
+        }
+        return true
+    }
+
+}
