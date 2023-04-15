0c72c707756bb274b85c152cc73b34f2b2914498
Raphael Feitosa <raphaelcs2@gmail.com>
2023-04-15 19:10:25 -0300

    fix: importing FeignConfiguration

--- src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/RestWithSpringBootKotlinAndHexagonalApplication.kt
+++ src/main/kotlin/br/com/hexagonal/restwithspringbootkotlinandhexagonal/RestWithSpringBootKotlinAndHexagonalApplication.kt
@@ -1,13 +1,16 @@
 package br.com.hexagonal.restwithspringbootkotlinandhexagonal
 
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.cloud.openfeign.FeignAutoConfiguration
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 @EnableFeignClients
+@ImportAutoConfiguration(FeignAutoConfiguration::class)
 class RestWithSpringBootKotlinAndHexagonalApplication
 
 fun main(args: Array<String>) {
